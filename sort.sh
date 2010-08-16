0fe62b1fe5cfa2e04739112f3cc0b821843d3129
Őry, Máté <orymate@ubuntu.com>
2010-08-16 09:44:12 +0200

    $1 => $1 nem menő

--- sort.sh
+++ sort.sh
@@ -2,7 +2,9 @@
 sort -u lista.dat > lista.dat.
 mv lista.dat. lista.dat
 grep -v '.* => .*[^ ]' lista.dat && exit 1
-awk 'BEGIN {FS=" => "}elozo==$1{print "kétszer van: ", $1; exit 2}{elozo=$1}'\
-   lista.dat || exit 2
+awk 'BEGIN {FS=" => "}
+     elozo == $1 {print "kétszer van: ", $1; exit 2}
+     $1 == $2 {print "wtf?", $0}
+     {elozo = $1}' lista.dat || exit 2
 exit 0
 
